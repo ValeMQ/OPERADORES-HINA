67236f9729bad0e732df3adbcad31e4afd9d9748
ValeMQ <144612009+ValeMQ@users.noreply.github.com>
2024-04-14 13:13:35 -0400

    tarea de operadores nomas es hina

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = 4
+let b =  50
+let c = 6
+
+if (a >= b && a >= c) {
+    console.log("El número más grande es: " + a);
+} else if (b >= a && b >= c) {
+    console.log("El número más grande es: " + b);
+} else {
+    console.log("El número más grande es: " + c);
+}
